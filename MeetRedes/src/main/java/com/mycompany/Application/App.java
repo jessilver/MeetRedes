452ac145ce283e17f5c225ce1033c8c66130f264
Jesse Silva <jesse1eliseu@gmail.com>
2024-04-11 00:19:45 -0300

    Second commit

--- MeetRedes/src/main/java/com/mycompany/Application/App.java
+++ MeetRedes/src/main/java/com/mycompany/Application/App.java
@@ -1,29 +1,28 @@
 package com.mycompany.Application;
 
 import com.mycompany.Clients.Cliente;
-// import java.net.*;
+import java.net.*;
 import java.util.*;
 import java.io.*;
-import java.util.Scanner;
 
 public class App {
     public static void main( String args[] ) throws IOException {
+
+        Map<String, Cliente> clientes = new HashMap<>();
 
         String ip;
         int porta;
         Scanner sc = new Scanner(System.in);
 
-        Map<String, Cliente> clientes = new HashMap<>();
-
         System.out.print("Digite o ip: ");
         ip=sc.next();
         System.out.print("Digite a porta: ");
         porta=sc.nextInt();
-
-        Cliente novoCliente = new Cliente(ip, porta);
+        
+        Cliente novoCliente = new Cliente(porta);
 
         clientes.put(ip, novoCliente);
-        clientes.get(ip).conectar();
+        clientes.get(ip).conectar(ip);
         sc.close();
     }
 }
--- MeetRedes/src/main/java/com/mycompany/Clients/Cliente.java
+++ MeetRedes/src/main/java/com/mycompany/Clients/Cliente.java
@@ -2,32 +2,51 @@ package com.mycompany.Clients;
 
 import java.net.*;
 import java.io.*;
+import java.util.UUID;
 
-public class Cliente {
+public class Cliente implements Runnable{
 
-    private String ip;
-    private int porta;
-    private Socket socket;
+    private final int porta;
+    private final Socket socket;
+    private final String id;
+    private  DataInputStream entrada;
+    private  DataOutputStream saida;
 
-    public Cliente(String ip, int porta) {
-        this.ip = ip;
+    public Cliente(int porta) throws IOException {
+        this.socket = new Socket();
         this.porta = porta;
+        this.id = UUID.randomUUID().toString();
     }
 
-    public void conectar() throws IOException {
+    public void conectar(String ip) throws IOException {
         try {
-            InetSocketAddress endereco = new InetSocketAddress(this.ip, this.porta);
-            socket = new Socket();
-            socket.connect(endereco, 1000);
+            InetSocketAddress endereco = new InetSocketAddress(ip, this.porta);
+            this.socket.connect(endereco, 1000);
             System.out.println("Ip: "+ip+" conectado ao servidor na porta: "+porta);
+            
+            this.entrada = new DataInputStream(socket.getInputStream());
+            this.saida = new DataOutputStream(socket.getOutputStream());
         } catch (IOException e) {
             System.out.println("Nao foi possivel se conectar ao servidor: "+e);
         }
     }
 
+    public void enviarMensagem(String mensagem) throws IOException {
+        if (socket.isConnected()) {
+            saida.writeUTF(mensagem);
+            System.out.println("Cliente " + id + " enviou mensagem: " + mensagem);
+        } else {
+            System.out.println("Erro: Cliente não conectado ao servidor!");
+        }
+    }
+
     
     public String getIp() {
-        return this.ip;
+        return this.socket.getInetAddress().getHostAddress();
+    }
+
+    public String getId() {
+        return this.id;
     }
 
     public int getPorta() {
@@ -36,5 +55,14 @@ public class Cliente {
 
     public Socket getSocket() {
         return this.socket;
+    }
+
+    public void closeSocket() throws IOException{
+        this.socket.close();
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
     }
 }
